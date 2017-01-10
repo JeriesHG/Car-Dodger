58c8f000ac40ad18515494a43e94a8c3189df0e6
Jeries Handal <JeriesHG@gmail.com>
2017-01-10 16:18:45 -0600

    Added linecast and road deactivation

---
+++ Assets/Scripts/RoadDeactivation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadDeactivation : MonoBehaviour
+{
+	void Update ()
+	{
+		RaycastHit2D hitInfo = GetComponent<DrawLine> ().hitInfo;
+		if (hitInfo.collider != null)
+			this.gameObject.SetActive (false);
+	}
+}

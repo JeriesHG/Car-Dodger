90818922d79ffb86aac6c4643d9a8f35e8242eac
Jeries Handal <JeriesHG@gmail.com>
2017-01-13 16:06:51 -0600

    AI & Scene changes
    Added Car AI prefabs
    Added HardAI basic functionality

---
+++ Assets/Scripts/CarAI.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarAI : MonoBehaviour
+{
+
+	public float detectPlayerDistance = 2f;
+	public float carSpeed = 0.5f;
+	public GameObject playerPrefab;
+
+	// Use this for initialization
+	void Start ()
+	{
+		playerPrefab = GameObject.FindGameObjectWithTag ("Player");
+	}
+
+
+	void Update ()
+	{
+
+	}
+}
---
+++ Assets/Scripts/HardAI.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HardAI : CarAI
+{
+
+	void Update ()
+	{
+		if (Vector3.Distance (this.transform.position, playerPrefab.transform.position) < detectPlayerDistance) {
+			Vector3 delta = (playerPrefab.transform.position - transform.position).normalized;
+			float moveSpeed = carSpeed * Time.deltaTime;
+			transform.position = new Vector3 ((transform.position.x + (delta.x * moveSpeed)), transform.position.y);
+		}
+	}
+}
